b6419f9528333e8f7dee8559881462ccfe268508
Leonardo Souza <leogsouza@gmail.com>
2024-08-31 01:03:15 -0300

    implement create and list users

--- backend/api/user.go
+++ backend/api/user.go
@@ -1,8 +1,12 @@
 package api
 
 import (
+	"time"
+
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	db "leogsouza.dev/superfin/db/sqlc"
+	"leogsouza.dev/superfin/utils"
 )
 
 type userHandler struct {
@@ -22,10 +26,6 @@ func (u *userHandler) RegisterRoutes() {
 }
 
 func (u *userHandler) listUsers(c *fiber.Ctx) error {
-	return c.JSON([]string{"user1", "user2"})
-}
-
-func (u *userHandler) createUser(c *fiber.Ctx) error {
 	arg := db.ListUsersParams{
 		Offset: 0,
 		Limit:  10,
@@ -35,5 +35,65 @@ func (u *userHandler) createUser(c *fiber.Ctx) error {
 	if err != nil {
 		return err
 	}
-	return c.JSON(users)
+
+	usersResponse := []*userResponse{}
+
+	for _, user := range users {
+		ur := transformDbUsertoUserResponse(&user)
+		usersResponse = append(usersResponse, ur)
+	}
+
+	return c.JSON(usersResponse)
+}
+
+type createUserParams struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,gte=8"`
+}
+
+type userResponse struct {
+	ID        int64     `json:"id"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (u *userHandler) createUser(c *fiber.Ctx) error {
+	var userParams createUserParams
+
+	// parse body content into struct
+	if err := c.BodyParser(&userParams); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	// validate the data
+	validate := validator.New()
+	if err := validate.Struct(userParams); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+
+	hashedPassword, err := utils.GenerateHashPassword(userParams.Password)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	userDb := db.CreateUserParams{}
+	userDb.Email = userParams.Email
+	userDb.Password = hashedPassword
+	user, err := u.server.queries.CreateUser(c.Context(), userDb)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	}
+	userResponse := transformDbUsertoUserResponse(&user)
+
+	return c.Status(fiber.StatusOK).JSON(userResponse)
+}
+
+func transformDbUsertoUserResponse(dbUser *db.User) *userResponse {
+	return &userResponse{
+		ID:        dbUser.ID,
+		Email:     dbUser.Email,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+	}
 }
